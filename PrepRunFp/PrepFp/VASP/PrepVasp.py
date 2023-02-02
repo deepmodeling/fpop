922fb85b9b3d4f57a7ca8fdf4476742e4ea581f0
Chenqqian Zhang <100290172+Chengqian-Zhang@users.noreply.github.com>
2023-02-02 17:55:59 +0800

    Update PrepVasp.py

--- PrepRunFp/PrepFp/VASP/PrepVasp.py
+++ PrepRunFp/PrepFp/VASP/PrepVasp.py
@@ -1,6 +1,3 @@
-import sys,os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),"../../../")))
-
 from PrepRunFp.PrepFp.PrepFp import PrepFp
 from PrepRunFp.PrepFp.VASP.VaspInputs import VaspInputs
 import dpdata
