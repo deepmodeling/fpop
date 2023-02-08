db5ee83de659d45e6f12fee835c984f302527150
Chengqian-Zhang <2000011006@stu.pku.edu.cn>
2023-02-08 17:43:58 +0800

    Update UT

--- tests/test_prep_run_vasp.py
+++ tests/test_prep_run_vasp.py
@@ -1,6 +1,24 @@
+'''
+If you want to do the UTs with dflow.You need export some environment variables on your machine.
+export DFLOW_HOST=https://workflows.deepmodeling.com
+export DFLOW_K8S_API_SERVER=https://workflows.deepmodeling.com
+export DFLOW_S3_REPO_KEY=oss-bohrium
+export DFLOW_S3_STORAGE_CLIENT=dflow.plugins.bohrium.TiefblueClient
+export BOHRIUM_USERNAME=<bohrium-email>
+export BOHRIUM_PASSWORD=<bohrium-password>
+export BOHRIUM_PROJECT_ID=<bohrium-project-id>
+
+If you don't want to do the UTs with dflow:
+export SKIP_UT_WITH_DFLOW=1
+'''
+
+from dflow import config, s3_config
+from dflow.plugins import bohrium
+from dflow.plugins.bohrium import TiefblueClient
 import os
 import numpy as np
 import unittest
+import warnings
 
 from dflow import (
     InputParameter,
@@ -23,101 +41,42 @@ from dflow.python import (
     OPIO,
     OPIOSign,
     Artifact,
+    upload_packages,
 )
 
 import time, shutil, json, jsonpickle
-from typing import Set, List
 from pathlib import Path
 
-from context import PrepRunFp
-from context import upload_python_packages
+from context import (
+    PrepRunFp,
+    default_image,
+    upload_python_packages,
+    skip_ut_with_dflow,
+    skip_ut_with_dflow_reason,
+    )
 from PrepRunFp.PrepFp.VASP.PrepVasp import PrepVasp
 from PrepRunFp.PrepFp.VASP.VaspInputs import VaspInputs
 from mocked_ops import MockedRunVasp
 from PrepRunFp.PrepRunFp import PrepRunFp
+upload_packages.append("../PrepRunFp")
+upload_packages.append("./context.py")
 
 default_config = {
         "prep":{
             "template_config" : {
-                "image" : 'registry.dp.tech/dptech/prod-11461/phonopy:v1.2',
+                "image" : default_image,
             },  
         },
         "run":{
             "template_config" : {
-                "image" : 'registry.dp.tech/dptech/prod-11461/phonopy:v1.2',
+                "image" : default_image,
             },  
         },
     }
 
-def check_vasp_tasks(tcase, ntasks):
-    cc = 0
-    tdirs = []
-    for ii in range (ntasks):
-        tdir = "task.%06d" % cc
-        tdirs.append(tdir)
-        print(tdir)
-        tcase.assertTrue(Path(tdir).is_dir())
-        incar = Path(tdir)/'INCAR'
-        poscar = Path(tdir)/'POSCAR'
-        potcar = Path(tdir)/'POTCAR'
-        kpoints = Path(tdir)/'KPOINTS'
-        tcase.assertTrue(incar.is_file())
-        tcase.assertTrue(poscar.is_file())
-        tcase.assertTrue(potcar.is_file())
-        tcase.assertTrue(kpoints.is_file())
-        tcase.assertEqual(incar.read_text(),'This is INCAR')
-        tcase.assertEqual(potcar.read_text(),'This is POTCAR')
-        cc += 1
-    return tdirs
-
-class TestPrepVaspDpConf(unittest.TestCase):
-    '''
-    Deepmd/npy format named ["data.000","data.001"].
-    No optional_input or optional_artifact.
-    '''
-    def setUp(self):
-        self.ntasks = 3
-        self.confs = [Path('data.000'),Path('data.001')]
-        self.incar = Path('incar')
-        self.incar.write_text('This is INCAR')
-        self.potcar = Path('potcar')
-        self.potcar.write_text('This is POTCAR')
-        self.type_map = ['Na']
-
-    def tearDown(self):
-        for ii in range(self.ntasks):
-            work_path = Path("task.%06d"%ii)
-            if work_path.is_dir():
-                shutil.rmtree(work_path)
-        for ii in [self.incar,self.potcar]:
-            if ii.is_file:
-                os.remove(ii)
-
-    def test(self):
-        op = PrepVasp()
-        vasp_inputs = VaspInputs(
-            0.3,
-            self.incar,
-            {'Na':self.potcar},
-            True,
-        )
-        out = op.execute(
-            OPIO(
-                {
-                    "config" : {},
-                    "optional_input" : {},
-                    "confs" : self.confs,
-                    "inputs" : vasp_inputs,
-                    "type_map" : self.type_map,
-                }
-            )
-        )
-        tdirs = check_vasp_tasks(self, self.ntasks)
-        self.assertEqual(tdirs, out['task_names'])
-        self.assertEqual(tdirs, [str(ii) for ii in out['task_paths']])
-
 class TestMockedRunVasp(unittest.TestCase):
     def setUp(self):
+        warnings.simplefilter('ignore', ResourceWarning)
         self.ntask = 6
         self.task_list = []
         for ii in range(self.ntask):
@@ -170,31 +129,45 @@ class TestMockedRunVasp(unittest.TestCase):
             self.assertTrue(Path(out['backward_dir'] / 'my_log').is_file())
             self.check_run_vasp_output(self.task_list_str[ii])
 
+@unittest.skipIf(skip_ut_with_dflow, skip_ut_with_dflow_reason)
 class TestPrepRunVasp(unittest.TestCase):
     def setUp(self):
+        warnings.simplefilter('ignore', ResourceWarning)
         self.ntasks = 3
-        self.confs = ["data.000","data.001"]
+        self.confs = [Path(Path('confs')/'data.000'),Path(Path('confs')/'data.001')]
         self.incar = Path('incar')
         self.incar.write_text("This is INCAR")
         self.potcar = Path('potcar')
         self.potcar.write_text('This is POTCAR')
         self.type_map = ['Na']
-    '''
+        self.optional_testfile = Path('test_file')
+        self.optional_testfile.write_text('This is an optional artifact')
+
+    def check_prep_run_vasp_output(
+        self,
+        task_name : str,
+    ):
+        cwd = os.getcwd()
+        os.chdir(task_name)
+        self.assertTrue(Path("my_backward").is_dir())
+        for ii in ['POSCAR','POTCAR','TEST','my_log']:
+            self.assertTrue(Path(Path("my_backward")/ii).is_file())
+        os.chdir(cwd)
+    
     def tearDown(self):
         for ii in range(self.ntasks):
             work_path = Path("task.%06d" % ii)
             if work_path.is_dir():
                 shutil.rmtree(work_path)
-        for ii in [self.incar, self.potcar]:
+        for ii in [self.incar, self.potcar, self.optional_testfile]:
             if ii.is_file():
                 os.remove(ii)
-    '''
+
     def test(self):
         steps = PrepRunFp(
             "prep-run-vasp",
             PrepVasp,
             MockedRunVasp,
-            upload_python_packages = upload_python_packages,
             prep_config = default_config["prep"],
             run_config = default_config["run"],
         )
@@ -212,11 +185,13 @@ class TestPrepRunVasp(unittest.TestCase):
                 'config' : default_config,
                 'inputs' : vasp_inputs,
                 'optional_input' : {},
-                'backward_list' : ['POSCAR','POTCAR'],
+                'backward_list' : ['POSCAR','POTCAR','TEST'],
+                'backward_dir_name' : 'my_backward',
+                'log_name' : 'my_log',
             },
             artifacts = {
-                "confs" : self.confs,
-                "optional_artifact" : {},
+                "confs" : upload_artifact(self.confs),
+                "optional_artifact" : upload_artifact({'TEST':Path("test_file")}),
             },
         )
         
@@ -227,8 +202,10 @@ class TestPrepRunVasp(unittest.TestCase):
         while wf.query_status() in ["Pending", "Running"]:
             time.sleep(4)
 
-        self.assertEqual(wf.query_status(), "Succeeded")
+        assert(wf.query_status() == 'Succeeded')
         step = wf.query_step(name="prep-run-step")[0]
-        self.assertEqual(step.phase, "Succeeded")
-
         download_artifact(step.outputs.artifacts["backward_dirs"])
+        
+        task_names = ["task.%06d" % ii for ii in range(self.ntasks)]
+        for ii in task_names:
+            self.check_prep_run_vasp_output(ii)
