3c405f4cecb87bd50acf591e94877e7ae3fa8fc0
Chengqian-Zhang <2000011006@stu.pku.edu.cn>
2023-02-15 10:23:10 +0800

    pass CICD

--- tests/test_prep_vasp.py
+++ tests/test_prep_vasp.py
@@ -74,7 +74,7 @@ def dump_conf_from_poscar(
         for ii in range(len(conf_list)):
             ls = dpdata.System("POSCAR_%d"%ii, fmt="vasp/poscar")
             ls.to_deepmd_npy("data.%03d"%ii)
-            confs.append("data.%03d")
+            confs.append("data.%03d"%ii)
             os.remove("POSCAR_%d"%ii)
         return confs
     elif type == "vasp/poscar":
