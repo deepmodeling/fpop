d1fd0904c78861141f6e5a91882a7b0bb25c82d8
Chengqian Zhang <20000110006@stu.pku.edu.cn>
2023-03-23 17:24:16 +0800

    test

--- fpop/utils/step_config.py
+++ fpop/utils/step_config.py
@@ -1,11 +1,10 @@
-import os
-from dflow import config
+import os, dflow
 from dflow.plugins.dispatcher import DispatcherExecutor
 
 def init_executor(
     executor_dict,
 ):
-    if executor_dict is None or config["mode"] == "debug":
+    if executor_dict is None or dflow.config["mode"] == "debug":
         return None
     etype = executor_dict.pop("type")
     if etype == "dispatcher":
