044f54f051b55b8b77e45dade7ce8ef24b69cadb
Andy6M <xieqiming1132@qq.com>
2024-06-25 18:17:51 +0800

    Fpop support for CP2K (Bohrium)

---
+++ examples/cp2k/preprunvasp.py
@@ -0,0 +1,97 @@
+from dflow import (
+    Workflow,
+    Step,
+    upload_artifact,
+    download_artifact,
+    InputArtifact,
+    OutputArtifact,
+    ShellOPTemplate,
+)
+from dflow.python import (
+    PythonOPTemplate,
+    OP,
+    OPIO,
+    OPIOSign,
+    Artifact,
+    TransientError,
+    FatalError,
+    BigParameter,
+    upload_packages,
+)
+from pathlib import Path
+import fpop
+from fpop.cp2k import PrepCp2k,Cp2kInputs,RunCp2k
+from fpop.preprun_fp import PrepRunFp
+from dflow.plugins.dispatcher import DispatcherExecutor
+
+prep_template_config={}
+prep_step_config={}
+run_template_config={}
+run_slice_config={}
+run_step_config={
+        "executor":{
+            "type": "dispatcher",
+            "machine_dict":{
+                "batch_type": "Bohrium",
+                "context_type": "Bohrium",
+                "remote_profile": {
+                    "input_data": {
+                        "job_type": "container",
+                        "platform": "ali",
+                        "scass_type": "c16_m32_cpu",
+                        }
+                    }
+                }
+            }
+    }
+prep_image_config={}
+run_image_config={
+    "command": "ulimit -s unlimited && ulimit -m unlimited && mpirun -n 16 --allow-run-as-root --oversubscribe cp2k.popt -i input.inp",
+    }
+upload_python_packages=list(fpop.__path__)
+
+steps = PrepRunFp(
+    "prep-run-vasp",
+    PrepCp2k,
+    RunCp2k,
+    "registry.dp.tech/dptech/prod-15842/ubuntu:22.04-py3.10-ase",
+    "registry.dp.tech/dptech/prod-25571/initreaction:cp2k0410",
+    prep_template_config,
+    prep_step_config,
+    run_template_config,
+    run_slice_config,
+    run_step_config,
+    upload_python_packages, 
+)
+
+confs = ["POSCAR-1","POSCAR-2","POSCAR-3"]
+
+
+cp2k_inputs = Cp2kInputs('input.inp') # input files
+prep_run_step = Step(
+    'prep-run-step', 
+    template = steps,
+    parameters = {
+        'type_map' : ["Na"],
+        'prep_image_config' : prep_image_config,
+        'run_image_config' : run_image_config,
+        "log_name" : "output.log",
+        'inputs' : cp2k_inputs,
+        'optional_input' : {"conf_format":"vasp/poscar"},
+        'backward_list' : ["output.log"],
+    },
+    artifacts = {
+        "confs" : upload_artifact(confs),
+    },
+)
+
+wf = Workflow(name="preprunvasp")
+wf.add(prep_run_step)
+wf.submit()
+
+import time
+while wf.query_status() in ["Pending", "Running"]:
+    time.sleep(4)
+assert(wf.query_status() == 'Succeeded')
+step = wf.query_step(name="prep-run-step")[0]
+download_artifact(step.outputs.artifacts["backward_dirs"])
---
+++ fpop/cp2k.py
@@ -0,0 +1,229 @@
+from fpop.prep_fp import PrepFp
+from fpop.run_fp import RunFp
+import dpdata, sys, subprocess, os, shutil
+from ase.io import read, write
+from pathlib import Path
+from dflow.utils import run_command
+from typing import (
+    Any,
+    Tuple,
+    List,
+    Set,
+    Dict,
+    Optional,
+    Union,
+)
+import numpy as np
+from dargs import (
+    dargs, 
+    Argument, 
+    Variant, 
+    ArgumentEncoder,
+)
+from dflow.python import (
+    OP,
+    OPIO,
+    OPIOSign,
+    Artifact,
+    TransientError,
+    FatalError,
+    BigParameter,
+)
+
+class Cp2kInputs:
+    def __init__(self, inp_file: str):
+        """
+        Initialize the Cp2kInputs class.
+
+        Parameters
+        ----------
+        inp_file : str
+            The path to the user-submitted CP2K input file.
+        """
+        self.inp_file_from_file(inp_file)
+
+    @property
+    def inp_template(self):
+        """
+        Return the template content of the input file.
+        """
+        return self._inp_template
+
+    def inp_file_from_file(self, fname: str):
+        """
+        Read the content of the input file and store it.
+
+        Parameters
+        ----------
+        fname : str
+            The path to the input file.
+        """
+        self._inp_template = Path(fname).read_text()
+
+    @staticmethod
+    def args():
+        """
+        Define the arguments required by the Cp2kInputs class.
+        """
+        doc_inp_file = "The path to the user-submitted CP2K input file."
+        return [
+            Argument("inp_file", str, optional=False, doc=doc_inp_file),
+        ]
+
+class PrepCp2k(PrepFp):
+    def prep_task(
+            self,
+            conf_frame: dpdata.System,
+            cp2k_inputs: Cp2kInputs,
+            prepare_image_config: Optional[Dict] = None,
+            optional_input: Optional[Dict] = None,
+            optional_artifact: Optional[Dict] = None,
+    ):
+        """
+        Define how one CP2K task is prepared.
+
+        Parameters
+        ----------
+        conf_frame : dpdata.System
+            One frame of configuration in the dpdata format.
+        cp2k_inputs: Cp2kInputs
+            The Cp2kInputs object handles the input file of the task.
+        prepare_image_config: Dict, optional
+            Definition of runtime parameters in the process of preparing tasks.
+        optional_input: Dict, optional
+            Other parameters the developers or users may need.
+        optional_artifact: Dict[str, Path], optional
+            Other files that users or developers need.
+        """
+        # Generate POSCAR file from the configuration frame
+        conf_frame.to('vasp/poscar', 'POSCAR')
+
+        # Read the structure from the POSCAR file and write to a temporary XYZ file
+        atoms = read('POSCAR')
+        write('temp.xyz', atoms)
+
+        # Read the temporary XYZ file, remove the first two lines, and write to coord.xyz
+        with open('temp.xyz', 'r') as f:
+            lines = f.readlines()[2:]  # Remove the first two lines
+        with open('coord.xyz', 'w') as f:
+            f.writelines(lines)
+
+        # Generate CELL_PARAMETER file
+        cell_params = conf_frame['cells'][0]
+        with open('CELL_PARAMETER', 'w') as file:
+            file.write(f"A {cell_params[0,0]:14.8f} {cell_params[0,1]:14.8f} {cell_params[0,2]:14.8f}\n")
+            file.write(f"B {cell_params[1,0]:14.8f} {cell_params[1,1]:14.8f} {cell_params[1,2]:14.8f}\n")
+            file.write(f"C {cell_params[2,0]:14.8f} {cell_params[2,1]:14.8f} {cell_params[2,2]:14.8f}\n")
+
+        # Write the CP2K input file content
+        Path('input.inp').write_text(cp2k_inputs.inp_template)
+
+        # Copy optional files to the working directory
+        if optional_artifact:
+            for file_name, file_path in optional_artifact.items():
+                content = file_path.read_text()
+                Path(file_name).write_text(content)
+
+
+class RunCp2k(RunFp):
+    def input_files(self, task_path) -> List[str]:
+        """
+        The mandatory input files to run a CP2K task.
+        
+        Returns
+        -------
+        files: List[str]
+            A list of mandatory input file names.
+        """
+        return ["input.inp", "CELL_PARAMETER", "coord.xyz"]
+
+    def run_task(
+        self,
+        backward_dir_name,
+        log_name,
+        backward_list: List[str],
+        run_image_config: Optional[Dict] = None,
+        optional_input: Optional[Dict] = None,
+    ) -> str:
+        """
+        Defines how one FP task runs.
+
+        Parameters
+        ----------
+        backward_dir_name : str
+            The name of the directory which contains the backward files.
+        log_name : str
+            The name of log file.
+        backward_list : List[str]
+            The output files the users need. For example: ["output.log", "trajectory.xyz"]
+        run_image_config : Dict, optional
+            Keyword args defined by the developer. For example:
+            {
+              "command": "source /opt/intel/oneapi/setvars.sh && mpirun -n 64 /opt/cp2k/bin/cp2k.popt"
+            }
+        optional_input : Dict, optional
+            The parameters developers need in runtime. For example:
+            {
+                "conf_format": "cp2k/input"
+            }
+        
+        Returns
+        -------
+        backward_dir_name : str
+            The directory name which contains the files users need.
+        """
+        # Get the run command
+        if run_image_config:
+            command = run_image_config["command"]
+        else:
+            command = "cp2k" #这里不确定！！！！！
+        
+        # Run CP2K command and write output to log file
+        command = " ".join([command, ">", log_name])
+        kwargs = {"try_bash": True, "shell": True}
+        if run_image_config:
+            kwargs.update(run_image_config)
+            kwargs.pop("command", None)
+        
+        # Execute command
+        ret, out, err = run_command(command, raise_error=False, **kwargs)
+        if ret != 0:
+            raise TransientError(
+                "cp2k failed\n", "out msg", out, "\n", "err msg", err, "\n"
+            )
+        
+        # Check if the task was successful
+        if not self.check_run_success(log_name):
+            raise TransientError(
+                "cp2k failed, we could not check the exact cause. Please check the log file."
+            )
+        
+        # Create output directory and copy log file
+        os.makedirs(Path(backward_dir_name))
+        shutil.copyfile(log_name, Path(backward_dir_name) / log_name)
+        for ii in backward_list:
+            try:
+                shutil.copyfile(ii, Path(backward_dir_name) / ii)
+            except:
+                shutil.copytree(ii, Path(backward_dir_name) / ii)
+        
+        return backward_dir_name
+    
+    def check_run_success(self, log_name):
+        """
+        Check if the CP2K task ran successfully by examining the output file.
+
+        Returns
+        -------
+        success : bool
+            True if the task ran successfully with warnings line, False otherwise.
+        """
+        with open(log_name, "r") as f:
+            lines = f.readlines()
+        return any("The number of warnings for this run is" in line for line in lines)
+
+
+
+
+
+    
