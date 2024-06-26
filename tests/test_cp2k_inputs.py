bc16decf09a5d2c0338888d390b6ac81caf7b433
Andy6M <xieqiming1132@qq.com>
2024-06-26 17:46:35 +0800

    Add Unit test(UT) for Cp2k

---
+++ tests/test_cp2k_inputs.py
@@ -0,0 +1,24 @@
+from context import fpop
+import os,sys,json,glob,shutil,textwrap
+import dpdata
+import numpy as np
+import unittest
+from fpop.cp2k import Cp2kInputs
+from pathlib import Path
+
+class TestCP2KInputs(unittest.TestCase):
+    def setUp(self):
+        Path('template.inp').write_text('&GLOBAL\n  PROJECT foo\n&END GLOBAL\n')
+
+    def tearDown(self):
+        os.remove('template.inp')
+
+    def test_cp2k_input(self):
+        iinp_file = 'template.inp'
+        ci = Cp2kInputs(iinp_file)
+        self.assertEqual(ci.inp_template, '&GLOBAL\n  PROJECT foo\n&END GLOBAL\n')
+
+
+ 
+
+
