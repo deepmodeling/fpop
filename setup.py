b05b337590c31a5237b2dcbd9c0833b841c08cd4
Chenqqian Zhang <100290172+Chengqian-Zhang@users.noreply.github.com>
2024-07-01 17:11:36 +0800

    Update setup.py
    version 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fpop",
-    version="0.0.7",
+    version="0.0.8",
     author="Chengqian Zhang",
     author_email="2043899742@qq.com",
     description="Operators related to first-principles calculation",
